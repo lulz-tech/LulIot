e88a8c3a168193b34713e4b353a9c4fe9b0317f8
Alex Mubarakshin <muban96@mail.ru>
2019-06-29 00:08:17 +0300

    💄 Add tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 interface IAppProps { }
 
@@ -16,18 +16,18 @@ export default class App extends React.Component<IAppProps> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
