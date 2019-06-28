a258f93156f38da1c79feaf01f651ed52a53a349
Alex Mubarakshin <muban96@mail.ru>
2019-06-28 23:57:03 +0300

    🎉 Hello, motherfuckers! It's initial commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# LulIot
+
+Application for LulzTech's IoT project
+
+## Installation
+
+```bash
+# Clone repo
+git clone 
+cd ./
+
+# Install dependencies
+yarn
+
+# Run on iOs
+$ react-native run-ios
+```
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+interface IAppProps { }
+
+export default class App extends React.Component<IAppProps> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to LulIot!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
